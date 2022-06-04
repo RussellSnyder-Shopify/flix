f44a978025ba556bc9b19e6ff1f244dcd4f917d0
Russell <russell.snyder@shopify.com>
2022-06-04 07:30:14 +0200

    Editing events

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,23 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    movie_params =
+      params.require(:movie)
+            .permit(:title, :rating, :total_gross, :released_on, :description)
+
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
