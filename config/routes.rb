6ca10e2487f4c961b6e81cd364dc71d7e1c78ec9
Russell Snyder <russell@rustybass.comr>
2022-06-05 13:33:55 +0200

    User signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
   root 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :movies do
     resources :reviews
   end
+
+  resource :session, only: %i[new create destroy].freeze
+
+  resources :users
+
+  get 'signup' => 'users#new'
+  get 'signin' => 'sessions#new'
 end
