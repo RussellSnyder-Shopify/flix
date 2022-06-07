1a23884b4f6338f1af7ff508cb357c6c482e7b4e
Russell <russell.snyder@shopify.com>
2022-06-07 13:25:18 +0200

    CRUD for genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,7 @@
 class GenresController < ApplicationController
+  before_action :require_signin, except: %i[show]
+  before_action :require_admin, except: %i[show]
+
   def index
     @genre = Genre.new
     @genres = Genre.all
@@ -13,6 +16,27 @@ class GenresController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+
+    if @genre.update(genre_params)
+      redirect_to genres_path, notice: 'Genre updated created!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @genre = Genre.find(params[:id])
+
+    @genre.destroy
+    redirect_to genres_path(@movie), alert: 'Genre Deleted!'
   end
 
   private
