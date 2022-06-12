84a1cf0f830ebd152df8e729154a8ff58cd3da70
Russell Snyder <russell@rustybass.comr>
2022-06-12 15:11:30 +0200

    add first pagination test

--- spec/features/movies_spec.rb
+++ spec/features/movies_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Movies', type: :system do
   describe '#index' do
-    describe '- less than 10 movies' do
+    describe '- less than 20 movies' do
       it 'displays all movies' do
         FactoryBot.create_list(:movie, 8)
 
@@ -11,16 +11,26 @@ RSpec.describe 'Movies', type: :system do
         page.assert_selector('[data-test-id="movie-list"] > section', count: 8)
       end
 
-      it 'pagination is not shown '
+      it 'pagination is not shown' do
+        page.assert_selector('nav.pagy-nav', count: 0)
+      end
     end
 
-    describe 'Pagination' do
-      it 'shows first 10 movies if more than 10 exist'
-      it 'shows correct page highlighted'
-      it 'shows the correct page when a number is clicked'
-      it 'shows the correct page when next is clicked'
-      it 'shows the correct page when prev is clicked'
-      it 'prev is not shown if pagination is the first page'
+    describe '- more than 20 movies' do
+      describe 'Pagination' do
+        it 'shows first 20 movies if more than 30 exist' do
+          FactoryBot.create_list(:movie, 30)
+
+          visit movies_path
+
+          page.assert_selector('[data-test-id="movie-list"] > section', count: 20)
+        end
+        it 'shows correct page highlighted'
+        it 'shows the correct page when a number is clicked'
+        it 'shows the correct page when next is clicked'
+        it 'shows the correct page when prev is clicked'
+        it 'prev is not shown if pagination is the first page'
+      end
     end
   end
 end
