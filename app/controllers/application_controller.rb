196c80d48f4578b8ed47e7425d31012b81dcfb76
Russell Snyder <russell@rustybass.comr>
2022-06-12 19:54:35 +0200

    Basic translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 
   private
 
